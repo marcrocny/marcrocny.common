f6809da1d7659bd4944d05822db73969688b3e71
Marc Lewandowski <marcrocny@gmail.com>
2023-07-29 19:28:18 -0400

    Generic Ext - OrNullIf

---
+++ src/Common/Extensions/GenericExt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MarcRocNy.Common.Extensions
+{
+    public static class GenericExt
+    {
+        /// <summary>
+        /// Syntax sugar: use mainly to combine (very simple!) validation under a null-coalescing operator.
+        /// </summary>
+        public static T? OrNullIf<T>(this T? source, Func<T, bool> predicate)
+            => source == null ? source : predicate(source) ? default : source;
+    }
+}
---
+++ test/CommonTests/Extensions/GenericExtTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MarcRocNy.Common.Extensions
+{
+    public class GenericExtTests
+    {
+        [Theory]
+        [InlineData("valid", "valid")]
+        [InlineData(null,null)]
+        [InlineData("invalid", null)]
+        public void OrNullIf_Cases(string value, string expected)
+        {
+            value.OrNullIf(s => s == "invalid").Should().Be(expected);
+        }
+    }
+}
